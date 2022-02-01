6943f25ed1404a41e42513563121772474727a5b
Will Holmes <willholmes97@icloud.com>
2022-02-01 18:12:51 +0000

    Solution setup, Header created with a skeleton ui

---
+++ components/Header.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image'
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline'
+import { HomeIcon } from '@heroicons/react/solid'
+
+const Header = () => {
+  return (
+    <div className='shadow-sm border-b bg-white sticky top-0 z-50'>
+      <div className="mx-5 flex max-w-6xl justify-between lg:mx-auto">
+        <div className="relative hidden w-24 cursor-pointer lg:inline-grid">
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-10 flex-shrink-0 cursor-pointer lg:hidden">
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div className="max-w-xs">
+          <div className="relative mt-1 rounded-md p-3">
+            <div className="pointer-events-none absolute inset-y-0 flex items-center pl-3">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              className="block w-full rounded-md border-gray-300 bg-gray-50 pl-10 focus:border-black focus:ring-black sm:text-sm"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navButton" />
+          <MenuIcon className="h-6 cursor-pointer md:hidden" />
+          <div className="navButton relative">
+            <PaperAirplaneIcon className="navButton rotate-45" />
+            <div className="absolute -top-1 -right-2 flex h-5 w-5 animate-pulse items-center justify-center rounded-full bg-red-500 text-xs text-white">
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className="navButton" />
+          <UserGroupIcon className="navButton" />
+          <HeartIcon className="navButton" />
+          <img
+            src="https://res.cloudinary.com/practicaldev/image/fetch/s--XWtgdZei--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/539842/cd0a3b2a-7787-4dc9-8842-3a4781a8369b.jpg"
+            alt="Profile picture"
+            className="h-10 rounded-full"
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
