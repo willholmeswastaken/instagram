6fd3147ec1f504febd6a8a42bb4256748b08ea3b
Will Holmes <willholmes97@icloud.com>
2022-02-13 16:24:21 +0000

    Likes implemented

--- components/Post.tsx
+++ components/Post.tsx
@@ -12,10 +12,13 @@ import { useSession } from 'next-auth/react'
 import {
   addDoc,
   collection,
+  deleteDoc,
+  doc,
   onSnapshot,
   orderBy,
   query,
   serverTimestamp,
+  setDoc,
   Timestamp,
 } from 'firebase/firestore'
 import { db } from '../firebase'
@@ -30,11 +33,16 @@ interface PostProps {
 }
 
 interface Comment {
-  id: string;
+  id: string
   comment: string
   username: string
-  userImage: string;
-  timestamp: Timestamp;
+  userImage: string
+  timestamp: Timestamp
+}
+
+interface Like {
+  id: string
+  username: string
 }
 
 const Post: FunctionComponent<PostProps> = ({
@@ -47,7 +55,10 @@ const Post: FunctionComponent<PostProps> = ({
   const { data: session } = useSession()
   const [currentComment, setCurrentComment] = useState<string>('')
   const [comments, setComments] = useState<Array<Comment>>([])
+  const [likes, setLikes] = useState<Array<Like>>([])
+  const [hasLiked, setHasLiked] = useState<boolean>(false)
   const commentButtonDisabled = !currentComment.trim()
+
   useEffect(
     () =>
       onSnapshot(
@@ -55,14 +66,49 @@ const Post: FunctionComponent<PostProps> = ({
           collection(db, 'posts', id, 'comments'),
           orderBy('timestamp', 'desc')
         ),
-        (snapshot) => setComments(snapshot.docs.map((x) => ({
-          id: x.id,
-          ...x.data()
-        } as Comment)))
+        (snapshot) =>
+          setComments(
+            snapshot.docs.map(
+              (x) =>
+                ({
+                  id: x.id,
+                  ...x.data(),
+                } as Comment)
+            )
+          )
       ),
-    [db]
+    [db, id]
   )
 
+  useEffect(
+    () =>
+      onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
+        setLikes(
+          snapshot.docs.map(
+            (x) =>
+              ({
+                id: x.id,
+                ...x.data(),
+              } as Like)
+          )
+        )
+      ),
+    [db, id]
+  )
+
+  useEffect(() => {
+    setHasLiked(likes.some((x) => x.id === session?.user?.email))
+  }, [likes])
+
+  const likePost = async () => {
+    if (hasLiked) {
+      await deleteDoc(doc(db, 'posts', id, 'likes', session?.user?.email!))
+    } else {
+      await setDoc(doc(db, 'posts', id, 'likes', session?.user?.email!), {
+        username: session?.user?.name,
+      })
+    }
+  }
 
   const sendComment = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
@@ -93,7 +139,14 @@ const Post: FunctionComponent<PostProps> = ({
       {session && (
         <div className="flex justify-between px-4 pt-4">
           <div className="flex space-x-4">
-            <HeartIcon className="btn" />
+            {hasLiked ? (
+              <HeartIconFilled
+                onClick={() => likePost()}
+                className="btn text-red-500"
+              />
+            ) : (
+              <HeartIcon onClick={() => likePost()} className="btn" />
+            )}
             <ChatIcon className="btn" />
             <PaperAirplaneIcon className="btn" />
           </div>
@@ -103,27 +156,35 @@ const Post: FunctionComponent<PostProps> = ({
 
       <p className="truncate p-5">
         {/* TODO: add the show more on captions */}
+        {likes.length > 0 && (
+          <p className="mb-1 font-bold">
+            {likes.length} like{likes.length > 1 ? 's' : ''}
+          </p>
+        )}
         <span className="mr-1 font-bold">{username} </span>
         {caption}
       </p>
       {/* comments */}
-        {comments.length > 0 && (
-          <div className='ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin'>
-            {comments.map(x => (
-              <div key={x.id} className='flex items-center space-x-2 mb-3'>
-                <img src={x.userImage} alt="posted users image" className='h-7 rounded-full' />
-                <p className="text-sm flex-1">
-                  <span className="font-bold">{x.username} </span>
-                  {x.comment}
-                </p>
-                <Moment fromNow className="pr-5 text-xs">
-                  {x.timestamp.toDate()}
-                </Moment>
-              </div>
-            ))}
-          </div>
-        )}
-
+      {comments.length > 0 && (
+        <div className="ml-10 h-20 overflow-y-scroll scrollbar-thin scrollbar-thumb-black">
+          {comments.map((x) => (
+            <div key={x.id} className="mb-3 flex items-center space-x-2">
+              <img
+                src={x.userImage}
+                alt="posted users image"
+                className="h-7 rounded-full"
+              />
+              <p className="flex-1 text-sm">
+                <span className="font-bold">{x.username} </span>
+                {x.comment}
+              </p>
+              <Moment fromNow className="pr-5 text-xs">
+                {x.timestamp.toDate()}
+              </Moment>
+            </div>
+          ))}
+        </div>
+      )}
 
       {session && (
         <form className="flex items-center p-4" action="">
